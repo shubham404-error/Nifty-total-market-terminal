75d1777c11e2409eeae67625f7ec86cea2686f14
shubham404-error <shubhamtejani29@gmail.com>
2026-09-15 04:27:44 +0530

    Cleanup fix scripts

--- fix_engine5.py
+++
@@ -1,7 +0,0 @@
-﻿with open("engine.py", "r", encoding="utf-8") as f:
-    content = f.read()
-
-content = content.replace("Has_252d_History", "HistoryEligible")
-
-with open("engine.py", "w", encoding="utf-8") as f:
-    f.write(content)
